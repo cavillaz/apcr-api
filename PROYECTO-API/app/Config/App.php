f4b0d2ab04524ac1ae8ce3527706db453cc08958
Camilo Villa <andresvillaz@gmail.com>
2024-11-26 21:00:26 -0500

    Configurar BDD en GCP

--- PROYECTO-API/app/Config/App.php
+++ PROYECTO-API/app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://34.170.125.148:8080/';
+    public string $baseURL = 'http://34.170.125.148/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
