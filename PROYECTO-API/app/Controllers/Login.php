9f728271e46d0c5775eb92eadcfa0f8e0d02527e
Camilo Villa <andresvillaz@gmail.com>
2024-11-28 21:35:26 -0500

    Consumo de mobile

--- PROYECTO-API/app/Controllers/Login.php
+++ PROYECTO-API/app/Controllers/Login.php
@@ -72,7 +72,7 @@ class Login extends BaseController
 
     private function setCorsHeaders()
     {
-        header("Access-Control-Allow-Origin: https://api.softnerdapcr.icu"); // Cambiar "*" por el dominio del cliente en producción
+        header("Access-Control-Allow-Origin: https://api.softnerdapcr.icu/"); // Cambiar "*" por el dominio del cliente en producción
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
         header("Access-Control-Allow-Credentials: true"); // Si estás manejando cookies o tokens
